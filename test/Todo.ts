bc43414bd5482bd9640ebaa8507be9bc86ace87e
Victoria <adedayovicky123@gmail.com>
2024-02-13 16:06:58 +0100

    add test for update todo

--- test/Todo.ts
+++ test/Todo.ts
@@ -13,14 +13,33 @@ describe("Todo", function () {
   describe("Creating, updating, toggling, and deleting todos", function () {
     it("Should create a todo", async function () {
       const { todoContract } = await loadFixture(deployTodoContractFixture);
+
       await todoContract.createTodo(
         "Wash Clothes",
         "You must wash all clothes"
       );
+
       const [title, description, isDone] = await todoContract.getTodo(0);
       expect(title).to.equal("Wash Clothes");
       expect(description).to.equal("You must wash all clothes");
       expect(isDone).to.equal(false);
     });
+  });
+
+  it("Should update a todo", async function () {
+    const { todoContract } = await loadFixture(deployTodoContractFixture);
+
+    await todoContract.createTodo("Wash Clothes", "You must wash all clothes");
+
+    await todoContract.updateTodo(
+      0,
+      "Don't Wash Clothes",
+      "You must not wash all clothes"
+    );
+
+    const [title, description, isDone] = await todoContract.getTodo(0);
+    expect(title).to.equal("Don't Wash Clothes");
+    expect(description).to.equal("You must not wash all clothes");
+    expect(isDone).to.equal(false);
   });
 });
